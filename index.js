5d98853e64f08157185d97d1e13a45f39fc404c9
Nokwanda Rose Ngwenya <129734873+kwanda1999@users.noreply.github.com>
2024-02-13 16:48:35 +0200

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald'
+  },
+  {
+    title: 'Wuthering Heights',
+    author: 'Emily Bronte',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
